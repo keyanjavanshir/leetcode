981b9e3a1c383d56113df31871100dbe4b0562f4
Keyan Javanshir <keyanjavanshir@live.dk>
2024-12-13 22:38:37 +0100

    Starting first challenge and setting up proj

---
+++ src/problem-1480/src/RunningSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+public class Solution {
+
+    public int[] RunningSum(int[] nums) {
+
+        int[] runningArray = [];
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            runningArray = nums[i];
+        }
+
+        return nums;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("I am running....");
+
+        int[] array = [1, 2, 3, 4, 5]; // [1, 3, 6, 10, 15]
+
+        Solution solution = new Solution();
+        solution.RunningSum(array);
+    }
+}
